0f6e0bf26be99243439fd9756d7cdcdc0d747246
Ashish Singh <ashishsingh21798@gmail.com>
2024-05-27 07:15:35 +0530

    Added css

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,8 @@
 export const getPost = async() => {
     const response = await fetch(
-        "https://jsonplaceholder.typicode.com/posts"
-    )
+        "https://jsonplaceholder.typicode.com/posts",{
+            method:"GET",
+        })
     return await response.json()
 }
 
